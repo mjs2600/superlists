2f28bc7f2300fef51aeeb979d8f10d9ab66d093d
Michael Simpson <mjs2600@gmail.com>
2022-05-03 17:59:21 -0400

    Initial commit

---
+++ functional_test.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+
+def test_getting_the_page_title():
+    browser = webdriver.Firefox()
+    browser.get("http://localhost:8000")
+
+    assert "To-Do" in browser.title
+
+    browser.quit()
