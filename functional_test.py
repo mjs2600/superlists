0652b453713f53cebae5f718e04baf2fc9164555
Michael Simpson <michael@higherorder.fun>
2022-05-06 18:13:31 -0400

    Extend functional test

--- functional_test.py
+++ functional_test.py
@@ -1,10 +1,18 @@
 from selenium import webdriver
+import pytest
 
 
-def test_getting_the_page_title():
-    browser = webdriver.Firefox()
+@pytest.fixture
+def browser():
+    browser = webdriver.Firefox()  # type: ignore
+    yield browser
+    browser.quit()
+
+
+def test_can_start_a_list_and_retrieve_it_later(browser):
     browser.get("http://localhost:8000")
 
     assert "To-Do" in browser.title
 
-    browser.quit()
+    header_text = browser.find_element_by_tag_name("h1").text
+    assert "To-Do" in header_text
